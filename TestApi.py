034f03557f9aa8789a28a00b548eeafdaa1292aa
Chaeli Vieira <cvieira@stevens.edu>
2021-03-16 21:37:16 -0400

    hw5a

--- TestApi.py
+++ TestApi.py
@@ -1,26 +1,49 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-
+import json
 from api import repoApi
 from api import checkRepo
+from api import getCommits
+from unittest.mock import Mock, patch
+
+
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
 
 class TestApi(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
+    @patch('api.requests.get')
+    def test_repoApi(self, mock_get):
+     
+        with open('./mockJsons/repos.json') as f:
+            data = json.load(f)
+        a = data
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = a
+        response = repoApi("chaelivieira")
+        #print(response)
+        self.assertEqual(response,['546Project', 'cs146-example', 'cs392--perm_sec_b', 'help', 'joanielovesHP', 'lab3', 'MARCK', 'SSW567', 'SSW567GitHubApi', 'SSW567HW2a'])
+    
+    @patch('api.requests.get')
+    def test_commits(self, mock_get):
+        with open('./mockJsons/hw2a.json') as f:
+            data = json.load(f)
+        k = data
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = k
+        response = getCommits("chaelivieira", "SSW567HW2a")
+        self.assertEqual(response,"Repo:SSW567HW2a : Number of Commits: 12")
 
-    def testValidID(self): 
-        self.assertEqual(repoApi("chaelivieira"),['Repo:546Project : Number of Commits: 1', 'Repo:cs146-example : Number of Commits: 8', 'Repo cs392--perm_sec_b is empty', 'Repo:help : Number of Commits: 3', 'Repo:joanielovesHP : Number of Commits: 1', 'Repo lab3 is empty', 'Repo:MARCK : Number of Commits: 30', 'Repo:SSW567 : Number of Commits: 4', 'Repo:SSW567GitHubApi : Number of Commits: 5', 'Repo:SSW567HW2a : Number of Commits: 11'] )
-    def testInvalidID(self): 
-        self.assertEqual(repoApi("chaelvieira"),{'message': 'Not Found', 'documentation_url': 'https://docs.github.com/rest/reference/repos#list-repositories-for-a-user'} )
-    def testValidRepo(self):
-        self.assertEqual(checkRepo("chaelivieira", "MARCK"), 30)
-    def testInvalidRepo(self):
-        self.assertEqual(checkRepo("chaelivieira","invalidRepo"), {'message': 'Not Found', 'documentation_url': 'https://docs.github.com/rest/reference/repos#list-commits'})
-    def testEmptyRepo(self):
-        self.assertEqual(checkRepo("chaelivieira", "lab3"),{'message': 'Git Repository is empty.', 'documentation_url': 'https://docs.github.com/rest/reference/repos#list-commits'})
-
+    @patch('api.requests.get')
+    def test_invalid_repo(self, mock_get):
+        with open('./mockJsons/errorRepo.json') as f:
+            data = json.load(f)
+        k = data
+        mock_get.return_value.json.return_value = k
+        response = repoApi("chaelivieira")
+        self.assertEqual(response,{'message': 'Not Found', 'documentation_url': 'https://docs.github.com/rest/reference/repos#list-repositories-for-a-user'} )
+       
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
--- api.py
+++ api.py
@@ -5,40 +5,36 @@ import json
 def repoApi(id):
     response1 = requests.get("https://api.github.com/users/{}/repos".format(id))
     obj = response1.json()
-    #print(response1)
+    #print(response1.json())
     if(response1.status_code == 200):
         list = []
-        output = []
         for x in obj:
             list.append( x["name"])
-        #print(list)
-        for y in list:
-            response2 = requests.get("https://api.github.com/repos/{}/{}/commits".format(id,y))
-            if(response2.status_code == 200):
-                num = len(response2.json())
-                #print(num)
-                s = "Repo:{} : Number of Commits: {}".format(y, num)
-                #print(s)
-                output.append(s)
-            else:
-                s = "Repo {} is empty".format(y)
-                output.append(s)
-                print(s)
-                print(response2.json())
-        print("##########OUTPUT##########")
-        print (output)
-        return output
+        return list
     else:
         print(response1.json())
         return response1.json()
-def checkRepo(id, repo):
-    response = requests.get("https://api.github.com/repos/{}/{}/commits".format(id,repo))
-    if(response.status_code == 200):
-        return len(response.json())
-    else:
-        return response.json()
+def getCommits(id, y):
+    response2 = requests.get("https://api.github.com/repos/{}/{}/commits".format(id,y))
+    if(response2.status_code == 200):
+        num = len(response2.json())
 
+        #print(response2.json())
+        s = "Repo:{} : Number of Commits: {}".format(y, num)
+        #print(s)
+        return s
+    else:
+        s = "Repo {} is empty".format(y)
+        return s
+        #print(s)
+        #print(response2.json())
+def output(id):
+    list = repoApi(id)
+    output = []
+    for y in list:
+            output.append(getCommits(id,y))
+    return output
 if __name__ == '__main__':
- 
-  #repoApi("chaelivieira")
-  print(checkRepo("chaelivieira", "lab3"))
+    print("running tests")
+    print(repoApi("chaelivieira"))
+    #print(checkRepo("chaelivieira", "MARCK"))
